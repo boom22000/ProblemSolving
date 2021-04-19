a3dbbe5107c55476d8df3c1b846aada010d5a59f
SungBeom.Cho <F1322@dpwah.com>
2021-04-19 15:35:50 +0900

    setup

---
+++ environmentTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace cSharpPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = { "test", "hello", "Sung`s", "Git", "Welcome guys" };
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.WriteLine("{0}", s[i]);
+            }
+        }
+    }
+}
